c4757ba8ecf259185ad1cd8f3fd0210364afd832
chenxu <chenxu@mail.ustc.edu.cn>
2022-08-23 11:56:23 +0800

    add

--- sh1_set.sh
+++ sh1_set.sh
@@ -6,7 +6,7 @@
 # Last Modified Date: 6 2022-08-20 22:50:15
 # Last Modified By  : chenxu <chenxu@mail.ustc.edu.cn>
 
-
+USER1=chenxu
 apt install -y git
 git clone https://github.com/cxwx/blocks
 echo change password and create new user now!!!!
@@ -14,3 +14,5 @@ cp sshd_config /etc/ssh/sshd_config
 chmod 644 /etc/ssh/sshd_config
 cp hosts.deny /etc/hosts.deny
 chmod 644 /etc/hosts.deny
+cp -r /root/.ssh /home/${USER1}
+chown -R /home/${USER1}/.ssh
