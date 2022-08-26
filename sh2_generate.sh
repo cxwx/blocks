5ff2db196af9fbf3dac7d2f5e0c285bff389ea2f
chenxu <chenxu@mail.ustc.edu.cn>
2022-08-26 16:37:31 +0800

    add sort bug

--- sh2_generate.sh
+++ sh2_generate.sh
@@ -6,13 +6,13 @@
 #    By: chenxu <chenxu@mail.ustc.edu.cn>           +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/08/23 12:01:57 by chenxu            #+#    #+#              #
-#    Updated: 2022/08/25 15:30:54 by chenxu           ###   ########.fr        #
+#    Updated: 2022/08/26 16:37:16 by chenxu           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 git pull
 lastb |awk '{print $3}' |grep "\." |sort |uniq |awk '{printf ("ALL: %s\n", $1)}'>newblock
-cat newblock hosts.deny |uniq >allblock
+cat newblock hosts.deny |sort |uniq >allblock
 mv allblock hosts.deny
 rm newblock
 cp hosts.deny /etc/hosts.deny
